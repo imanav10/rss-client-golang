2a7b210b5a45c330ebd1218c9c459530a671c99f
Manav <101001298+imanav10@users.noreply.github.com>
2024-06-09 23:10:04 +0530

    chore: Add dependency on godotenv v1.5.1 for environment variable management

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/imanav10/rss-client-golang
 
 go 1.22.4
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -1,7 +1,20 @@
-package main 
+package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"github.com/joho/godotenv"
+)
 
 func main() {
+
 	fmt.Println("hello Wrld!")
+	godotenv.Load()
+	portString := os.Getenv("PORT")
+	if portString == "" {
+		log.Fatal("PORT not found in variable")
+	}
+
+	fmt.Println("PORT:",portString)
 }
