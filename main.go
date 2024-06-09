b1f822dcef6d21c99d4043852cfb33694002ee5d
Manav <101001298+imanav10@users.noreply.github.com>
2024-06-10 02:33:21 +0530

    chore: Update go.mod and go.sum with new dependencies

--- main.go
+++ main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"os"
+	"net/http"
 	"github.com/joho/godotenv"
+	"github.com/go-chi/cors"
+	"github.com/go-chi/chi"
 )
 
 func main() {
 
-	fmt.Println("hello Wrld!")
 	godotenv.Load()
 	portString := os.Getenv("PORT")
 	if portString == "" {
 		log.Fatal("PORT not found in variable")
 	}
+	router := chi.NewRouter()
 
-	fmt.Println("PORT:",portString)
+router.Use(cors.Handle(cors.Options{
+	AllowedOrigins: []string{"https://*","http://"},
+	AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	AllowedHeaders: []string{"*"},
+	AllowCredentials: false,
+	MaxAge: 300, // Maximum value not ignored by any of major browsers
+}))
+
+	srv := &http.Server{
+		Handler: router,
+		Addr: ":"+ portString,
+	}
+
+	log.Printf("serving passing throught that port %v:",portString)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
 }
